1bdd5ba8be90bbbc30e52f56b07deb57f0fa8ef2
Arihant-303 <arihantjain101@gmail.com>
2026-09-13 18:05:57 +0530

    first commit

---
+++ test_llm.py
@@ -0,0 +1,6 @@
+def test_basic_functionality():
+    # This is a placeholder test function to demonstrate basic functionality.
+    message = "Hello, world!"
+
+    assert message is not None
+    assert len(message) > 0
